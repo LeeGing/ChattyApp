5c19753a56032d8f25f3ef7e482959583e1eb4a0
Edmond Lee <e.lee@live.ca>
2017-08-24 01:18:28 +0000

    Server folder added / Username now changable

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import ChatBar from './ChatBar.jsx'
 import NavBar from './NavBar.jsx'
 
 const ChattyData = {
-currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-messages: [
-  { id: "1",
-    username: "Bob",
-    content: "Has anyone seen my marbles?",
-  },
-  { id: "2",
-    username: "Anonymous",
-    content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-  }
-]
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    { id: "1",
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    { id: "2",
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
 }
 
 class App extends Component {
@@ -28,28 +28,16 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.socket = new WebSocket('ws://localhost:3001');
-    // this.socket.addEventListener('open', () => {
-    //   this.socket.send('it works');
-    // });
+    this.socket = new WebSocket('ws://localhost:3001')
+    
     this.socket.addEventListener('message', (event) => {
-      // console.log('Got a message');
-      // console.log(event.data);
+      console.log(event.data);
+
       const newMessages = this.state.messages;
       const messageObject = JSON.parse(event.data);
       newMessages.push(messageObject);
       this.setState({messages: newMessages});
     });
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-     console.log("Simulating incoming message");
-     // Add a new message to the list of messages in the data store
-     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-     const messages = this.state.messages.concat(newMessage)
-     // Update the state of the app component.
-     // Calling setState will trigger a call to render() in App and all child components.
-     this.setState({messages: messages})
-   }, 3000);
   }
 
   sendMessage(text){
@@ -61,14 +49,20 @@ class App extends Component {
     this.socket.send(JSON.stringify(newMessage));
   }
 
+  changeUsername(newUsername){
+   this.setState({currentUser: newUsername});
+   //TO DO : Send a system message saying that the user changed their name.
+  }
+
   render() {
     return (
       <div>
           <NavBar/>
           <MessageList messages={ this.state.messages}/>
-          <ChatBar user={ this.state.currentUser} sendMessage={text => this.sendMessage(text)}/>
+          <ChatBar sendMessage={text => this.sendMessage(text)} changeUsername={text => this.changeUsername(text)}/>
       </div>
     );
   }
 }
+
 export default App;
